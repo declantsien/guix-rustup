f240037fd38b5c6ac07817d36f436257c349e227
Declan Tsien <declantsien@riseup.net>
2025-03-22 19:40:41 +0800

    Add rust 1.85.1

--- guix/rustup/packages/rust.scm
+++ guix/rustup/packages/rust.scm
@@ -485,7 +485,10 @@
 (define-public rust-toolchain-1.85.0
   (rustup "1.85.0"))
 
-(define-public rust-toolchain rust-toolchain-1.85.0)
+(define-public rust-toolchain-1.85.1
+  (rustup "1.85.1"))
+
+(define-public rust-toolchain rust-toolchain-1.85.1)
 
 (define-public rust-bin
   (deprecated-package "rust-bin" rust-toolchain))
