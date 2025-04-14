2d647ae418a1c4be71a78edfcfcc0819648d5418
Eval EXEC <execvy@gmail.com>
2025-04-14 16:08:45 +0800

    Add 1.86.0: `guile -L guix ./scripts/update.scm 1.86.0`

--- guix/rustup/packages/rust.scm
+++ guix/rustup/packages/rust.scm
@@ -488,7 +488,10 @@
 (define-public rust-toolchain-1.85.1
   (rustup "1.85.1"))
 
-(define-public rust-toolchain rust-toolchain-1.85.1)
+(define-public rust-toolchain-1.86.0
+  (rustup "1.86.0"))
+
+(define-public rust-toolchain rust-toolchain-1.86.0)
 
 (define-public rust-bin
   (deprecated-package "rust-bin" rust-toolchain))
