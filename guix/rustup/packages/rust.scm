3a4c867f36de6fb957a5b0f4749db30eb4edc336
Declan Tsien <declantsien@riseup.net>
2026-02-01 10:17:33 +0800

    name rust-bin has be deprecated and removed

--- guix/rustup/packages/rust.scm
+++ guix/rustup/packages/rust.scm
@@ -2,467 +2,335 @@
   #:use-module (guix packages)
   #:use-module (rustup build toolchain))
 
-(define-public rust-bin-1.1.0
+(define-public rust-toolchain-1.1.0
   (rustup "1.1.0" #:components '(rust)))
 
-(define-public rust-bin-1.2.0
+(define-public rust-toolchain-1.2.0
   (rustup "1.2.0" #:components '(rust)))
 
-(define-public rust-bin-1.3.0
+(define-public rust-toolchain-1.3.0
   (rustup "1.3.0" #:components '(rust)))
 
-(define-public rust-bin-1.4.0
+(define-public rust-toolchain-1.4.0
   (rustup "1.4.0" #:components '(rust)))
 
-(define-public rust-bin-1.5.0
+(define-public rust-toolchain-1.5.0
   (rustup "1.5.0" #:components '(rust)))
 
-(define-public rust-bin-1.6.0
+(define-public rust-toolchain-1.6.0
   (rustup "1.6.0" #:components '(rust)))
 
-(define-public rust-bin-1.7.0
+(define-public rust-toolchain-1.7.0
   (rustup "1.7.0" #:components '(rust)))
 
-(define-public rust-bin-1.8.0
+(define-public rust-toolchain-1.8.0
   (rustup "1.8.0"))
 
-(define-public rust-bin-1.9.0
+(define-public rust-toolchain-1.9.0
   (rustup "1.9.0"))
 
-(define-public rust-bin-1.10.0
+(define-public rust-toolchain-1.10.0
   (rustup "1.10.0"))
 
-(define-public rust-bin-1.11.0
+(define-public rust-toolchain-1.11.0
   (rustup "1.11.0"))
 
-(define-public rust-bin-1.12.0
+(define-public rust-toolchain-1.12.0
   (rustup "1.12.0"))
 
-(define-public rust-bin-1.12.1
+(define-public rust-toolchain-1.12.1
   (rustup "1.12.1"))
 
-(define-public rust-bin-1.13.0
+(define-public rust-toolchain-1.13.0
   (rustup "1.13.0"))
 
-(define-public rust-bin-1.14.0
+(define-public rust-toolchain-1.14.0
   (rustup "1.14.0"))
 
-(define-public rust-bin-1.15.0
+(define-public rust-toolchain-1.15.0
   (rustup "1.15.0"))
 
-(define-public rust-bin-1.15.1
+(define-public rust-toolchain-1.15.1
   (rustup "1.15.1"))
 
-(define-public rust-bin-1.16.0
+(define-public rust-toolchain-1.16.0
   (rustup "1.16.0"))
 
-(define-public rust-bin-1.17.0
+(define-public rust-toolchain-1.17.0
   (rustup "1.17.0"))
 
-(define-public rust-bin-1.18.0
+(define-public rust-toolchain-1.18.0
   (rustup "1.18.0"))
 
-(define-public rust-bin-1.19.0
+(define-public rust-toolchain-1.19.0
   (rustup "1.19.0"))
 
-(define-public rust-bin-1.20.0
+(define-public rust-toolchain-1.20.0
   (rustup "1.20.0"))
 
-(define-public rust-bin-1.21.0
+(define-public rust-toolchain-1.21.0
   (rustup "1.21.0"))
 
-(define-public rust-bin-1.22.0
+(define-public rust-toolchain-1.22.0
   (rustup "1.22.0"))
 
-(define-public rust-bin-1.22.1
+(define-public rust-toolchain-1.22.1
   (rustup "1.22.1"))
 
-(define-public rust-bin-1.23.0
+(define-public rust-toolchain-1.23.0
   (rustup "1.23.0"))
 
-(define-public rust-bin-1.24.0
+(define-public rust-toolchain-1.24.0
   (rustup "1.24.0"))
 
-(define-public rust-bin-1.24.1
+(define-public rust-toolchain-1.24.1
   (rustup "1.24.1"))
 
-(define-public rust-bin-1.25.0
+(define-public rust-toolchain-1.25.0
   (rustup "1.25.0"))
 
-(define-public rust-bin-1.26.0
+(define-public rust-toolchain-1.26.0
   (rustup "1.26.0"))
 
-(define-public rust-bin-1.26.1
+(define-public rust-toolchain-1.26.1
   (rustup "1.26.1"))
 
-(define-public rust-bin-1.26.2
+(define-public rust-toolchain-1.26.2
   (rustup "1.26.2"))
 
-(define-public rust-bin-1.27.0
+(define-public rust-toolchain-1.27.0
   (rustup "1.27.0"))
 
-(define-public rust-bin-1.27.1
+(define-public rust-toolchain-1.27.1
   (rustup "1.27.1"))
 
-(define-public rust-bin-1.27.2
+(define-public rust-toolchain-1.27.2
   (rustup "1.27.2"))
 
-(define-public rust-bin-1.28.0
+(define-public rust-toolchain-1.28.0
   (rustup "1.28.0"))
 
-(define-public rust-bin-1.29.0
+(define-public rust-toolchain-1.29.0
   (rustup "1.29.0"))
 
-(define-public rust-bin-1.29.1
+(define-public rust-toolchain-1.29.1
   (rustup "1.29.1"))
 
-(define-public rust-bin-1.29.2
+(define-public rust-toolchain-1.29.2
   (rustup "1.29.2"))
 
-(define-public rust-bin-1.30.0
+(define-public rust-toolchain-1.30.0
   (rustup "1.30.0"))
 
-(define-public rust-bin-1.30.1
+(define-public rust-toolchain-1.30.1
   (rustup "1.30.1"))
 
-(define-public rust-bin-1.31.0
+(define-public rust-toolchain-1.31.0
   (rustup "1.31.0"))
 
-(define-public rust-bin-1.31.1
+(define-public rust-toolchain-1.31.1
   (rustup "1.31.1"))
 
-(define-public rust-bin-1.32.0
+(define-public rust-toolchain-1.32.0
   (rustup "1.32.0"))
 
-(define-public rust-bin-1.33.0
+(define-public rust-toolchain-1.33.0
   (rustup "1.33.0"))
 
-(define-public rust-bin-1.34.0
+(define-public rust-toolchain-1.34.0
   (rustup "1.34.0"))
 
-(define-public rust-bin-1.34.1
+(define-public rust-toolchain-1.34.1
   (rustup "1.34.1"))
 
-(define-public rust-bin-1.34.2
+(define-public rust-toolchain-1.34.2
   (rustup "1.34.2"))
 
-(define-public rust-bin-1.35.0
+(define-public rust-toolchain-1.35.0
   (rustup "1.35.0"))
 
-(define-public rust-bin-1.36.0
+(define-public rust-toolchain-1.36.0
   (rustup "1.36.0"))
 
-(define-public rust-bin-1.37.0
+(define-public rust-toolchain-1.37.0
   (rustup "1.37.0"))
 
-(define-public rust-bin-1.38.0
+(define-public rust-toolchain-1.38.0
   (rustup "1.38.0"))
 
-(define-public rust-bin-1.39.0
+(define-public rust-toolchain-1.39.0
   (rustup "1.39.0"))
 
-(define-public rust-bin-1.40.0
+(define-public rust-toolchain-1.40.0
   (rustup "1.40.0"))
 
-(define-public rust-bin-1.41.0
+(define-public rust-toolchain-1.41.0
   (rustup "1.41.0"))
 
-(define-public rust-bin-1.41.1
+(define-public rust-toolchain-1.41.1
   (rustup "1.41.1"))
 
-(define-public rust-bin-1.42.0
+(define-public rust-toolchain-1.42.0
   (rustup "1.42.0"))
 
-(define-public rust-bin-1.43.0
+(define-public rust-toolchain-1.43.0
   (rustup "1.43.0"))
 
-(define-public rust-bin-1.43.1
+(define-public rust-toolchain-1.43.1
   (rustup "1.43.1"))
 
-(define-public rust-bin-1.44.0
+(define-public rust-toolchain-1.44.0
   (rustup "1.44.0"))
 
-(define-public rust-bin-1.44.1
+(define-public rust-toolchain-1.44.1
   (rustup "1.44.1"))
 
-(define-public rust-bin-1.45.0
+(define-public rust-toolchain-1.45.0
   (rustup "1.45.0"))
 
-(define-public rust-bin-1.45.1
+(define-public rust-toolchain-1.45.1
   (rustup "1.45.1"))
 
-(define-public rust-bin-1.45.2
+(define-public rust-toolchain-1.45.2
   (rustup "1.45.2"))
 
-(define-public rust-bin-1.46.0
+(define-public rust-toolchain-1.46.0
   (rustup "1.46.0"))
 
-(define-public rust-bin-1.47.0
+(define-public rust-toolchain-1.47.0
   (rustup "1.47.0"))
 
 (define-public rust-toolchain-1.48.0
   (rustup "1.48.0"))
 
-(define-public rust-bin-1.48.0
-  (deprecated-package "rust-bin" rust-toolchain-1.48.0))
-
 (define-public rust-toolchain-1.49.0
   (rustup "1.49.0"))
-
-(define-public rust-bin-1.49.0
-  (deprecated-package "rust-bin" rust-toolchain-1.49.0))
 
 (define-public rust-toolchain-1.50.0
   (rustup "1.50.0"))
 
-(define-public rust-bin-1.50.0
-  (deprecated-package "rust-bin" rust-toolchain-1.50.0))
-
 (define-public rust-toolchain-1.51.0
   (rustup "1.51.0"))
-
-(define-public rust-bin-1.51.0
-  (deprecated-package "rust-bin" rust-toolchain-1.51.0))
 
 (define-public rust-toolchain-1.52.0
   (rustup "1.52.0"))
 
-(define-public rust-bin-1.52.0
-  (deprecated-package "rust-bin" rust-toolchain-1.52.0))
-
 (define-public rust-toolchain-1.52.1
   (rustup "1.52.1"))
-
-(define-public rust-bin-1.52.1
-  (deprecated-package "rust-bin" rust-toolchain-1.52.1))
 
 (define-public rust-toolchain-1.53.0
   (rustup "1.53.0"))
 
-(define-public rust-bin-1.53.0
-  (deprecated-package "rust-bin" rust-toolchain-1.53.0))
-
 (define-public rust-toolchain-1.54.0
   (rustup "1.54.0"))
-
-(define-public rust-bin-1.54.0
-  (deprecated-package "rust-bin" rust-toolchain-1.54.0))
 
 (define-public rust-toolchain-1.55.0
   (rustup "1.55.0"))
 
-(define-public rust-bin-1.55.0
-  (deprecated-package "rust-bin" rust-toolchain-1.55.0))
-
 (define-public rust-toolchain-1.56.0
   (rustup "1.56.0"))
-
-(define-public rust-bin-1.56.0
-  (deprecated-package "rust-bin" rust-toolchain-1.56.0))
 
 (define-public rust-toolchain-1.56.1
   (rustup "1.56.1"))
 
-(define-public rust-bin-1.56.1
-  (deprecated-package "rust-bin" rust-toolchain-1.56.1))
-
 (define-public rust-toolchain-1.57.0
   (rustup "1.57.0"))
-
-(define-public rust-bin-1.57.0
-  (deprecated-package "rust-bin" rust-toolchain-1.57.0))
 
 (define-public rust-toolchain-1.58.0
   (rustup "1.58.0"))
 
-(define-public rust-bin-1.58.0
-  (deprecated-package "rust-bin" rust-toolchain-1.58.0))
-
 (define-public rust-toolchain-1.58.1
   (rustup "1.58.1"))
-
-(define-public rust-bin-1.58.1
-  (deprecated-package "rust-bin" rust-toolchain-1.58.1))
 
 (define-public rust-toolchain-1.59.0
   (rustup "1.59.0"))
 
-(define-public rust-bin-1.59.0
-  (deprecated-package "rust-bin" rust-toolchain-1.59.0))
-
 (define-public rust-toolchain-1.60.0
   (rustup "1.60.0"))
-
-(define-public rust-bin-1.60.0
-  (deprecated-package "rust-bin" rust-toolchain-1.60.0))
 
 (define-public rust-toolchain-1.61.0
   (rustup "1.61.0"))
 
-(define-public rust-bin-1.61.0
-  (deprecated-package "rust-bin" rust-toolchain-1.61.0))
-
 (define-public rust-toolchain-1.62.0
   (rustup "1.62.0"))
-
-(define-public rust-bin-1.62.0
-  (deprecated-package "rust-bin" rust-toolchain-1.62.0))
 
 (define-public rust-toolchain-1.62.1
   (rustup "1.62.1"))
 
-(define-public rust-bin-1.62.1
-  (deprecated-package "rust-bin" rust-toolchain-1.62.1))
-
 (define-public rust-toolchain-1.63.0
   (rustup "1.63.0"))
-
-(define-public rust-bin-1.63.0
-  (deprecated-package "rust-bin" rust-toolchain-1.63.0))
 
 (define-public rust-toolchain-1.64.0
   (rustup "1.64.0"))
 
-(define-public rust-bin-1.64.0
-  (deprecated-package "rust-bin" rust-toolchain-1.64.0))
-
 (define-public rust-toolchain-1.65.0
   (rustup "1.65.0"))
-
-(define-public rust-bin-1.65.0
-  (deprecated-package "rust-bin" rust-toolchain-1.65.0))
 
 (define-public rust-toolchain-1.66.0
   (rustup "1.66.0"))
 
-(define-public rust-bin-1.66.0
-  (deprecated-package "rust-bin" rust-toolchain-1.66.0))
-
 (define-public rust-toolchain-1.66.1
   (rustup "1.66.1"))
-
-(define-public rust-bin-1.66.1
-  (deprecated-package "rust-bin" rust-toolchain-1.66.1))
 
 (define-public rust-toolchain-1.67.0
   (rustup "1.67.0"))
 
-(define-public rust-bin-1.67.0
-  (deprecated-package "rust-bin" rust-toolchain-1.67.0))
-
 (define-public rust-toolchain-1.67.1
   (rustup "1.67.1"))
-
-(define-public rust-bin-1.67.1
-  (deprecated-package "rust-bin" rust-toolchain-1.67.1))
 
 (define-public rust-toolchain-1.68.0
   (rustup "1.68.0"))
 
-(define-public rust-bin-1.68.0
-  (deprecated-package "rust-bin" rust-toolchain-1.68.0))
-
 (define-public rust-toolchain-1.68.1
   (rustup "1.68.1"))
-
-(define-public rust-bin-1.68.1
-  (deprecated-package "rust-bin" rust-toolchain-1.68.1))
 
 (define-public rust-toolchain-1.68.2
   (rustup "1.68.2"))
 
-(define-public rust-bin-1.68.2
-  (deprecated-package "rust-bin" rust-toolchain-1.68.2))
-
 (define-public rust-toolchain-1.69.0
   (rustup "1.69.0"))
-
-(define-public rust-bin-1.69.0
-  (deprecated-package "rust-bin" rust-toolchain-1.69.0))
 
 (define-public rust-toolchain-1.70.0
   (rustup "1.70.0"))
 
-(define-public rust-bin-1.70.0
-  (deprecated-package "rust-bin" rust-toolchain-1.70.0))
-
 (define-public rust-toolchain-1.71.0
   (rustup "1.71.0"))
-
-(define-public rust-bin-1.71.0
-  (deprecated-package "rust-bin" rust-toolchain-1.71.0))
 
 (define-public rust-toolchain-1.71.1
   (rustup "1.71.1"))
 
-(define-public rust-bin-1.71.1
-  (deprecated-package "rust-bin" rust-toolchain-1.71.1))
-
 (define-public rust-toolchain-1.72.0
   (rustup "1.72.0"))
-
-(define-public rust-bin-1.72.0
-  (deprecated-package "rust-bin" rust-toolchain-1.72.0))
 
 (define-public rust-toolchain-1.72.1
   (rustup "1.72.1"))
 
-(define-public rust-bin-1.72.1
-  (deprecated-package "rust-bin" rust-toolchain-1.72.1))
-
 (define-public rust-toolchain-1.73.0
   (rustup "1.73.0"))
-
-(define-public rust-bin-1.73.0
-  (deprecated-package "rust-bin" rust-toolchain-1.73.0))
 
 (define-public rust-toolchain-1.74.0
   (rustup "1.74.0"))
 
-(define-public rust-bin-1.74.0
-  (deprecated-package "rust-bin" rust-toolchain-1.74.0))
-
 (define-public rust-toolchain-1.74.1
   (rustup "1.74.1"))
-
-(define-public rust-bin-1.74.1
-  (deprecated-package "rust-bin" rust-toolchain-1.74.1))
 
 (define-public rust-toolchain-1.75.0
   (rustup "1.75.0"))
 
-(define-public rust-bin-1.75.0
-  (deprecated-package "rust-bin" rust-toolchain-1.75.0))
-
 (define-public rust-toolchain-1.76.0
   (rustup "1.76.0"))
-
-(define-public rust-bin-1.76.0
-  (deprecated-package "rust-bin" rust-toolchain-1.76.0))
 
 (define-public rust-toolchain-1.77.0
   (rustup "1.77.0"))
 
-(define-public rust-bin-1.77.0
-  (deprecated-package "rust-bin" rust-toolchain-1.77.0))
-
 (define-public rust-toolchain-1.77.1
   (rustup "1.77.1"))
-
-(define-public rust-bin-1.77.1
-  (deprecated-package "rust-bin" rust-toolchain-1.77.1))
 
 (define-public rust-toolchain-1.77.2
   (rustup "1.77.2"))
 
-(define-public rust-bin-1.77.2
-  (deprecated-package "rust-bin" rust-toolchain-1.77.2))
-
 (define-public rust-toolchain-1.78.0
   (rustup "1.78.0"))
-
-(define-public rust-bin-1.78.0
-  (deprecated-package "rust-bin" rust-toolchain-1.78.0))
 
 (define-public rust-toolchain-1.79.0
   (rustup "1.79.0"))
@@ -516,6 +384,3 @@
   (rustup "1.93.0"))
 
 (define-public rust-toolchain rust-toolchain-1.93.0)
-
-(define-public rust-bin
-  (deprecated-package "rust-bin" rust-toolchain))
